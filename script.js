c436e64b35abf19d2516045f14ad19c0042d061b
Anand Raj Bharti <anandrajbharti574@gmail.com>
2026-04-10 17:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// //your code here
 
-// let inp = document.querySelector("#newTodoInput");
-// let btn = document.querySelector("#addTodoBtn");
-// let ol = document.querySelector("#todoList")
-
-// btn.addEventListener("click", addElement); 
-
-// function addElement(){
-// 	// e.preventDefault();
-// 	let inputValue = inp.value;
-	
-//    let creatlist =  document.createElement("li");
-// 	creatlist.innerText = inputValue;
-// 		ol.append(creatlist);
-// 	inp.value = "";
-	
-// }
 
 
 let inp = document.querySelector("#newTodoInput");
